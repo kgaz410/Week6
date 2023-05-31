33062b98e9923d1a72e596f39dccfdb70d23aaa3
Kelsey Gazaway <Kelsey@Kelseys-MBP-7.attlocal.net>
2023-05-31 08:53:29 -0400

    final commit

--- APIPractice/index.js
+++ APIPractice/index.js
@@ -1,85 +1,75 @@
-    const prodContainer = document.getElementById("products-container")
-    const postContainer = document.getElementById("posts-container")    
-
-    const API_URL = "https://dummyjson.com/products"
-
-    const fetchAllProducts = async () => {
+    async function fetchProducts() {
         try {
-            const response = await fetch(API_URL)
-            const data = await response.json()
-            // console.log(data)
-
-            return data
-
+            let response = await fetch("https://dummyjson.com/products/");
+    
+            let translatedData = await response.json(); 
+    
+            let actualData = translatedData.results 
+            console.log(actualData)
+    
+           return actualData;
         } catch (error) {
-            console.log(error)
+          console.error(error);  
         }
     }
     
-    // fetchAllProducts()
-    
-    const renderAllProducts = (products) => {
-        products.forEach((products) => {
-            const prodElem = document.createElement("p")
 
-            prodElem.innerText = products.title
-            
-            prodContainer.appendChild(prodElem)
-        });
-
-
-
-    }
-
-
-
-    const init = async () => {
-        const products = await fetchAllProducts();
-        renderAllProducts(products)
-        console.log(products)
-
-
-    }
+    async function renderProducts() {
+        let prodContainer = document.getElementById("products-container")
     
-    init()
+        let myProducts = await fetchProducts();
     
-    
-    
-    
-    // async function fetchProducts() {
-    //     try {
-    //         let response = await fetch("https://dummyjson.com/products");
-    
-    //         let translatedData = await response.json(); 
-    
-    //         let actualData = translatedData.results 
-    
-    //        return actualData;
-    //     } catch (error) {
-    //       console.error(error);  
-    //     }
-    // }
-    
-
-    // async function renderData() {
-    //     let prodContainer = document.getElementById("products-container")
-    
-    //     let myProducts = await fetchProducts();
-    
-    //     for (let i = 0; i < myProducts.length; i++) {
+        for (let i = 0; i < myProducts.length; i++) {
          
-    //         let myCurrObj = myProducts[i];
+            let myCurrObj = myProducts[i];
             
-    //         let newElement = document.createElement("p");
+            let newElement = document.createElement("p");
     
-    //         newElement.innerText = myCurrObj.title;
-    //         newElement.style.border = "2px solid blue";
+            newElement.innerText = `${myCurrObj.title} ${myCurrObj.price}`
+            newElement.style.border = "3px solid blue";
     
-    //         prodContainer.appendChild(newElement)
-    //     }
-    // }
+            prodContainer.appendChild(newElement)
+        }
+    }
     
-    // renderData(); 
+    renderProducts(); 
+
+
+    async function fetchPostData() {
+        try {
+
+          let postResponse = await fetch('https://dummyjson.com/posts/');
+         
+          let translatedPostData = await postResponse.json();
+         
+          let actualPostData = translatedPostData;
+          console.log(actualPostData);
+         
+          return actualPostData;
+        } catch (error) {
+          console.error(error);
+          
+        }
+      }
+      
+      async function renderPost() {
+        let postContain = document.getElementById("posts-container");
+        let myPosts = await fetchPostData();
+      
+        for(let i = 0; i < myPosts.length; i++){
+            let myCurrPostObj= myPosts[i];
+          
+            let newPostEle = document.createElement('p');
+          
+            newPostEle.innerText = myCurrPostObj.title;
+          
+            newPostEle.style.border = "3px solid pink";
+          
+            postContain.appendChild(newPostEle);
+        };
+      }
+
+      renderPost()
 
 
 
